0b9175c63e0789e3b8d8c48a06c98b591ab346d4
Beibarss <96571576+Beibarss@users.noreply.github.com>
2022-02-21 09:50:08 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const paragraph = document.querySelector("p");
+paragraph.innerHTML = paragraph.innerText.split(' ').map(string => string.length > 8 ? 
+    `<span style="background:yellow">${string}</span>` : string).join(' ');
+paragraph.innerHTML = paragraph.innerHTML.split(/\.[^.<]/).join("<br>");
+
+paragraph.innerText = paragraph.innerText.split('').map((string) => {
+    if (string === "?") {
+        return string = "🤔";
+    } else if (string === "!") {
+        return string = "😮";
+    } else {
+        return string
+    }
+}).join("");
+const stringCount = paragraph.innerText.split(' ').length;
+const stringCountElem = document.createElement('div');
+stringCountElem.innerText = `Word count: ${stringCount}`;
+document.body.insertBefore(stringCountElem, paragraph);
+var div = document.getElementById('Link');
+var link = document.createElement('a');
+link.href = 'https://www.inc.com/jeff-haden/this-new-linkedin-study-reveals-top-8-job-interview-questions-and-how-great-job-candidates-answer-them.html';
+link.title = 'Source';
+link.appendChild(document.createTextNode('Source'));
+div.appendChild(link);
